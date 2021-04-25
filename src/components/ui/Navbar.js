d73698d64a3a8fbb191ed221b0acab636e77d919
Alex Camacho <camacho.ale94@gmail.com>
2021-04-25 12:46:08 -0500

    Add SearchScreen to Navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -14,15 +14,16 @@ export const Navbar = () => {
           <NavLink activeClassName="active" className="nav-item nav-link" exact to="/dc">
             DC
           </NavLink>
+          <NavLink activeClassName="active" className="nav-item nav-link" exact to="/search">
+            Search
+          </NavLink>
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
-        <ul className="navbar-nav ms-auto">
-          <NavLink activeClassName="active" className="nav-item nav-link" exact to="/login">
-            Logout
-          </NavLink>
-        </ul>
+      <div className="navbar-nav">
+        <NavLink activeClassName="active" className="nav-item nav-link" exact to="/login">
+          Logout
+        </NavLink>
       </div>
     </nav>
   )
