a978fa83951c5cdb108455ff7a4939406d6f4cbb
Alex Camacho <camacho.ale94@gmail.com>
2021-04-25 01:05:54 -0500

    Individual hero pages created

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './heroCard.css'
 
-export const HeroCard = ({ id, superhero, alter_ego, first_appearance, publisher, characters, }) => {
+export const HeroCard = ({ id, superhero, alter_ego, first_appearance, publisher, characters }) => {
   return (
-    <Link to={`./hero/${id}`} className="my-card">
+    <Link to={`./hero/${id}`} className="card">
       <img src={`./assets/heroes/${id}.jpg`} className="img img-responsive" alt={superhero}/>
       <div className="profile-name">{superhero}</div>
       <div className="profile-position">{alter_ego}</div>
@@ -13,7 +13,7 @@ export const HeroCard = ({ id, superhero, alter_ego, first_appearance, publisher
           <div className="row">
             <div className="col-ms-4">
               <h3>{publisher}</h3>
-              <p>Primera aparición: <br />{first_appearance}</p>
+              <p>Primera aparición: <br/>{first_appearance}</p>
               {
                 (alter_ego !== characters)
                 && <p>{characters}</p>
--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import { Redirect, useParams, useHistory } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById'
 
 export const HeroScreen = () => {
+  const { heroId: id } = useParams()
+  const hero = getHeroById(id)
+  let history = useHistory()
+  if (!hero) {
+    return <Redirect to="/"/>
+  }
+  const { superhero, alter_ego, first_appearance, publisher, characters } = hero
+  const handleReturn = () => {
+    if (history.length <= 2) {
+      history.push('/')
+    }
+    history.goBack()
+  }
+
   return (
-    <>
-      <h1>Hero Screen</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={`../assets/heroes/${id}.jpg`} alt={superhero} className="img-thumbnail"/>
+      </div>
+      <div className="col-8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><b>Alter ego: </b> {alter_ego}</li>
+          <li className="list-group-item"><b>Publisher: </b> {publisher}</li>
+          <li className="list-group-item"><b>First appearance: </b> {first_appearance}</li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+
+        <div className="d-flex justify-content-end">
+          <button className="btn btn-outline-dark btn-sm" onClick={handleReturn}>Return</button>
+        </div>
+      </div>
+    </div>
   )
 }
