4159e86463de3898b6b50f5741a1af2f942dec29
Alex Camacho <camacho.ale94@gmail.com>
2021-05-07 19:02:18 -0500

    Adding logic to private and public routes

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,5 @@
 
 D
 src/hooks/useHook.js,d/0/d063233218bb095695bb818868b49238e55332fc
+G
+src/auth/authReducer.js,e/f/ef12c0201775195fe7f8d68b1b7ec80d2eaadc44
--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../auth/AuthContext'
+import { types } from '../../types/types'
 
 export const LoginScreen = ({history}) => {
+
+  const { dispatch } = useContext(AuthContext)
+
   const handleLogin = () => {
-    history.replace('/')
+    const lastPath = localStorage.getItem('lastPath') || '/'
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'Alex'
+      }
+    })
+    history.replace(lastPath)
   }
   return (
     <div className="container mt-5">
       <h1>Login</h1>
       <hr/>
-      <button className="btn btn-outline-primary" onClick={handleLogin}>Log</button>
+      <button className="btn btn-outline-primary" onClick={handleLogin}>Login</button>
     </div>
   )
 }
